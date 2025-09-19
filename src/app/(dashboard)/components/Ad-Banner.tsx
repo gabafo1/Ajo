cea2479cafb8aa505f84f86a94c339ff6fd590a7
gabafo1 <gabrielafolabi@gmail.com>
2025-09-19 15:29:37 +0100

    nw

--- src/app/(dashboard)/components/Ad-Banner.tsx
+++ src/app/(dashboard)/components/Ad-Banner.tsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import Image from "next/image"
-import Link from "next/link"
-import { Card, CardContent, CardDescription, CardTitle } from "../../../components/ui/card"
-
-
-export function AdBanner() {
-  return (
-    <Card className=" h-[200px] overflow-hidden">
-        <Link href='#' className="block h-full">
-            <CardContent className="p-0 h-full flex flex-row items-center">
-                <div className="relative">
-                    <Image src="/alajo33.png" alt="Mobile app logo" width={200} height={200} objectFit="cover" />
-                </div>
-                <div className="p-4 w-1/2">
-                    <CardTitle>Download Our App!</CardTitle>
-                    <CardDescription>Get your analytics anywhere you go.</CardDescription>
-                </div>
-            </CardContent>
-        </Link>
-    </Card>
-  )
-=======
 import Link from "next/link";
 import Image from "next/image";
 import {
@@ -57,5 +34,4 @@ export function AdBanner() {
       </Link>
     </Card>
   );
->>>>>>> 978af04 (Initial commit)
 }
